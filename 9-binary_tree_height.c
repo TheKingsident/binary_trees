a2acc18bf9aaf92d61a5d1def329a29eec75b8c1
TheKingsident <kingsident@gmail.com>
2023-11-30 10:15:25 +0100

    Added new files

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,7 +14,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height;
 	size_t right_height;
 
-	left_height = right_height = 0;
+	left_height = 0;
+	right_height = 0;
 
 	if (tree->left != NULL)
 	{
